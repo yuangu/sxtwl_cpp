1ab110f0a7c0b64c7202fb8d8b179a56670725fe
yuangu <lifulinghan@aol.com>
2017-03-28 22:07:24 +0800

    初使化工程

---
+++ sxwnl/JD.cpp
@@ -0,0 +1,82 @@
+#include "JD.h"
+#include "const.h"
+#include <cstring>
+
+long double JD::DD2JD(int y, uint8_t m, long double d)
+{
+ 	int n = 0, G = 0;
+	//判断是否为格里高利历日1582*372+10*31+15
+	if (y * 372 + m * 31 + (int)(d) >= 588829)
+	{
+		G = 1;
+	}
+	if (m <= 2)
+	{
+		m += 12, y--;
+	}
+	//加百年闰
+	if (G)
+	{
+		n = (int)(y / 100), n = 2 - n + int(n / 4);
+	}
+	return (int)(365.25*(y + 4716)) + (int)(30.6001*(m + 1)) + d + n - 1524.5;
+}
+
+Time JD::JD2DD(int jd)
+{
+	Time r;
+	int D = (int)(jd + 0.5);
+	float F = jd + 0.5 - D, c;  //取得日数的整数部份A及小数部分F
+	if (D >= 2299161)
+	{
+		c = int((D - 1867216.25) / 36524.25), D += 1 + c - (int)(c / 4);
+	}
+	D += 1524;               r.Y = (int)((D - 122.1) / 365.25);//年数
+	D -= (int)(365.25*r.Y);   r.M = (int)(D / 30.601); //月数
+	D -= (int)(30.601*r.M);   r.D = D; //日数
+	if (r.M > 13)
+	{
+		r.M -= 13, r.Y -= 4715;
+	}
+	else
+	{
+		r.M -= 1, r.Y -= 4716;
+	}
+	//日的小数转为时分秒
+	F *= 24; r.h = (int)(F); F -= r.h;
+	F *= 60; r.m = (int)(F); F -= r.m;
+	F *= 60; r.s = F;
+	return r;
+}
+
+long double JD::toJD(Time& time)
+{
+	return JD::DD2JD(time.Y, time.M, time.D + ((time.s / 60 + time.m) / 60 + time.h) / 24);
+}
+
+//提取jd中的时间(去除日期);
+std::string JD::timeStr(long double jd)
+{
+	int h, m, s;
+	jd += 0.5; jd = (jd - int2(jd));
+	s = int2(jd * 86400 + 0.5);
+	h = int2(s / 3600); s -= h * 3600;
+	m = int2(s / 60);   s -= m * 60;
+	std::string ret = "";
+	char buff[11];
+	memset(buff, 0, 11);
+	sprintf_s(buff, "0%d", h);
+	ret.append(buff + strlen(buff) - 2);
+	ret += ":";
+
+	memset(buff, 0, 11);
+	sprintf_s(buff, "0%d", m);
+	ret.append(buff + strlen(buff) - 2);
+	ret += ":";
+
+	memset(buff, 0, 11);
+	sprintf_s(buff, "0%d", s);
+	ret.append(buff + strlen(buff) - 2);
+
+	return ret;
+}
---
+++ sxwnl/main.cpp
@@ -0,0 +1,63 @@
+#include "JD.h"
+#include <iostream>
+#include<vector>
+#include <conio.h>
+#include "const.h"
+#include "lunar.h"
+#include "eph.h"
+#include "SSQ.h"
+int main()
+{
+
+	static const char* Gan[] = { "甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸" };
+	static const char* Zhi[] = { "子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥" };
+	static const char* ShX[] = { "鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪" };
+	static const char* numCn[] = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十" }; //中文数字
+	static const char* jqmc[] = { "冬至", "小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏", "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑", "白露", "秋分", "寒露", "霜降", "立冬", "小雪", "大雪" };
+	static const char* ymc[] = { "十一", "十二", "正", "二", "三", "四", "五", "六", "七", "八", "九", "十" }; //月名称,建寅
+	static const char*  rmc[] = { "初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十", "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十", "廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十", "卅一" };
+
+
+	Lunar lunar;
+    Month month = lunar.yueLiCalc(2017, 2);
+	
+	std::cout  << month.y << "年"  << (int)month.m << "月" << std::endl;
+	std::cout << Gan[month.yearGan] << Zhi[month.yearZhi] << "年" << std::endl;
+	std::cout << "生肖：" << ShX[month.ShX] << std::endl;
+
+
+	std::vector<Day>& days = month.days;
+	for (auto it = days.begin(); it != days.end(); ++it)
+	{
+		Day& day = (*it);
+		std::cout << "===================================================" << std::endl;;
+		std::cout << "公历：" << day.y << "年" << (int)day.m << "月" << day.d << "日" << std::endl;
+		if (day.Lleap)
+		{
+			std::cout << "润" << ymc[day.Lmc] << "月" << rmc[day.Ldi] << "日" << std::endl;
+		}
+		else
+		{
+			std::cout << ymc[day.Lmc] << "月" << rmc[day.Ldi] << "日" << std::endl;
+		}
+		
+		std::cout << "儒略历:JD" << J2000 + day.d0 << std::endl;
+		std::cout << "星期" << numCn[day.week] << std::endl;
+		
+		std::cout << Gan[day.Lyear2.tg] << Zhi[day.Lyear2.dz] << "年" 
+			<< Gan[day.Lmonth2.tg] << Zhi[day.Lmonth2.dz] << "月"
+			<< Gan[day.Lday2.tg] << Zhi[day.Lday2.dz] << "日"
+			<< std::endl;
+		
+		std::cout << "距冬至" << day.cur_dz << "天" << std::endl;
+		std::cout << "距夏至" << day.cur_xz << "天" << std::endl;
+		std::cout << "距立秋" << day.cur_lq << "天" << std::endl;
+		std::cout << "距芒种" << day.cur_mz << "天" << std::endl;
+		std::cout << "距小暑" << day.cur_xs << "天" << std::endl;
+	
+
+
+	}
+
+	getchar();
+}
