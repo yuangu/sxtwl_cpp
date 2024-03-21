c7a58496291c4157d80e9aae86c6111915efb14a
yuangu <seantone@126.com>
2024-03-21 16:30:57 +0800

    c语言的新的导出接口

--- c/sxtwl_c.cpp
+++ c/sxtwl_c.cpp
@@ -1,293 +1,241 @@
-#include "sxtwl_c.h"
+#include "sxtwl_export.h"
+
 #include "sxtwl.h"
 
-extern "C"
+EXTERN_C_BEGIN
+
+sxtwl_Day *sxtwl_after(sxtwl_Day *day, sxtwl_Day *ret, int day_num)
 {
-	sxtwl_Day *sxtwl_after(sxtwl_Day *day, sxtwl_Day *ret, int day_num)
-	{
-		ret->ptr = (Day *)((Day *)day->ptr)->after(day_num);
-		return ret;
-	}
-
-	sxtwl_Day *sxtwl_before(sxtwl_Day *day, sxtwl_Day *ret, int day_num)
-	{
-		ret->ptr = (Day *)((Day *)day->ptr)->before(day_num);
-		return ret;
-	}
-
-	int sxtwl_getLunarDay(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getLunarDay();
-	}
-
-	int sxtwl_getLunarYear(sxtwl_Day *day, bool chineseNewYearBoundary)
-	{
-		return ((Day *)day->ptr)->getLunarYear(chineseNewYearBoundary);
-	}
-
-	sxtwl_GZ *sxtwl_getYearGZ(sxtwl_Day *day, sxtwl_GZ *ret, bool chineseNewYearBoundary)
-	{
-		GZ gz = ((Day *)day->ptr)->getYearGZ(chineseNewYearBoundary);
-		ret->dz = gz.dz;
-		ret->tg = gz.tg;
-		return ret;
-	}
-
-	sxtwl_GZ *sxtwl_getMonthGZ(sxtwl_Day *day, sxtwl_GZ *ret)
-	{
-		GZ gz = ((Day *)day->ptr)->getMonthGZ();
-		ret->dz = gz.dz;
-		ret->tg = gz.tg;
-		return ret;
-	}
-
-	sxtwl_GZ *sxtwl_getDayGZ(sxtwl_Day *day, sxtwl_GZ *ret)
-	{
-		GZ gz = ((Day *)day->ptr)->getDayGZ();
-		ret->dz = gz.dz;
-		ret->tg = gz.tg;
-		return ret;
-	}
-
-	sxtwl_GZ *sxtwl_getHourGZ(sxtwl_Day *day, sxtwl_GZ *ret, uint8_t hour, bool isZaoWanZiShi)
-	{
-		GZ gz = ((Day *)day->ptr)->getHourGZ(hour, isZaoWanZiShi);
-		ret->dz = gz.dz;
-		ret->tg = gz.tg;
-		return ret;
-	}
-
-	bool sxtwl_isLunarLeap(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->isLunarLeap();
-	}
-
-	int sxtwl_getSolarYear(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getSolarYear();
-	}
-
-	uint8_t sxtwl_getSolarMonth(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getSolarMonth();
-	}
-
-	int sxtwl_getSolarDay(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getSolarDay();
-	}
-
-	uint8_t sxtwl_getWeek(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getWeek();
-	}
-
-	// 处于该月的第几周
-	uint8_t sxtwl_getWeekIndex(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getWeekIndex();
-	}
-
-	//是否有节气
-	bool sxtwl_hasJieQi(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->hasJieQi();
-	}
-
-	// 获取节气
-	uint8_t sxtwl_getJieQi(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getJieQi();
-	}
-
-	double sxtwl_getJieQiJD(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getJieQiJD();
-	}
-
-	// 获取星座
-	uint8_t sxtwl_getConstellation(sxtwl_Day *day)
-	{
-		return ((Day *)day->ptr)->getConstellation();
-	}
-
-	void sxtwl_freeDay(sxtwl_Day *day)
-	{
-		delete ((Day *)day->ptr);
-	}
-
-	sxtwl_GZ *sxtwl_getShiGz(uint8_t dayTg, sxtwl_GZ *ret, uint8_t hour, bool isZaoWanZiShi)
-	{
-		GZ gz = sxtwl::getShiGz(dayTg, hour, isZaoWanZiShi);
-		ret->dz = gz.dz;
-		ret->tg = gz.tg;
-		return ret;
-	}
-
-	uint8_t sxtwl_getRunMonth(int By)
-	{
-		return sxtwl::getRunMonth(By);
-	}
-
-	uint8_t sxtwl_getLunarMonthNum(int By, uint8_t month, bool isRun)
-	{
-		return sxtwl::getLunarMonthNum(By, month, isRun);
-	}
-
-	sxtwl_Time *sxtwl_JD2DD(double jd, sxtwl_Time *ret)
-	{
-		Time t = sxtwl::JD2DD(jd);
-
-		ret->year = t.Y;
-		ret->month = t.M;
-		ret->day = t.D;
-
-		ret->hour = t.h;
-		ret->min = t.m;
-		ret->sec = t.s;
-		return ret;
-	}
-
-	double sxtwl_toJD(sxtwl_Time *time)
-	{
-		Time t(time->year, time->month, time->day, time->hour, time->min, time->sec);
-		return sxtwl::toJD(t);
-	}
-
-	sxtwl_Day *sxtwl_fromSolar(sxtwl_Day *ret, int year, uint8_t month, int day)
-	{
-		ret->ptr = (void *)sxtwl::fromSolar(year, month, day);
-		return ret;
-	}
-
-	sxtwl_Day *sxtwl_fromLunar(sxtwl_Day *ret, int year, uint8_t month, int day, bool isRun)
-	{
-		ret->ptr = (void *)sxtwl::fromLunar(year, month, day, isRun);
-		return ret;
-	}
-
-	double *sxtwl_siZhu2Year(double **list, int &ret_num, sxtwl_GZ *year, sxtwl_GZ *yue, sxtwl_GZ *ri, sxtwl_GZ *shi, int fromYear, int toYear)
-	{
-		auto ret = sxtwl::siZhu2Year(GZ(year->tg, year->dz), GZ(yue->tg, yue->dz), GZ(ri->tg, ri->dz), GZ(shi->tg, shi->dz), fromYear, toYear);
-		*list = (double *)malloc(ret.size() * sizeof(double));
-		memcpy(*list, &(ret[0]), ret.size() * sizeof(double));
-		return *list;
-	}
-
-	sxtwl_GZ *sxtwl_newGZ()
-	{
-		return (sxtwl_GZ *)struct_malloc(sizeof(sxtwl_GZ));
-	}
-
-	void sxtwl_freeGZ(sxtwl_GZ *gz)
-	{
-		struct_free(gz);
-	}
-
-	uint8_t sxtwl_GZ_getTg(sxtwl_GZ *gz)
-	{
-		return gz->tg;
-	}
-
-	uint8_t sxtwl_GZ_getDz(sxtwl_GZ *gz)
-	{
-		return gz->dz;
-	}
-
-	void sxtwl_GZ_setTg(sxtwl_GZ *gz, uint8_t tg)
-	{
-		gz->tg = tg;
-	}
-
-	void sxtwl_GZ_setDz(sxtwl_GZ *gz, uint8_t dz)
-	{
-		gz->dz = dz;
-	}
-
-	sxtwl_Time *sxtwl_newTime()
-	{
-		return (sxtwl_Time *)struct_malloc(sizeof(sxtwl_Time));
-	}
-
-	void sxtwl_freeTime(sxtwl_Time *time)
-	{
-		struct_free(time);
-	}
-
-	int sxtwl_Time_getYear(sxtwl_Time *time)
-	{
-		return time->year;
-	}
-
-	int sxtwl_Time_getMonth(sxtwl_Time *time)
-	{
-		return time->month;
-	}
-
-	int sxtwl_Time_getDay(sxtwl_Time *time)
-	{
-		return time->day;
-	}
-
-	void sxtwl_Time_setYear(sxtwl_Time *time, int value)
-	{
-		time->year = value;
-	}
-	void sxtwl_Time_setMonth(sxtwl_Time *time, int value)
-	{
-		time->month = value;
-	}
-	void sxtwl_Time_setDay(sxtwl_Time *time, int value)
-	{
-		time->day = value;
-	}
-
-	double sxtwl_Time_getHour(sxtwl_Time *time)
-	{
-		return time->hour;
-	}
-
-	double sxtwl_Time_getMin(sxtwl_Time *time)
-	{
-		return time->min;
-	}
-
-	double sxtwl_Time_getSec(sxtwl_Time *time)
-	{
-		return time->sec;
-	}
-
-	void sxtwl_Time_setHour(sxtwl_Time *time, double value)
-	{
-		time->hour = value;
-	}
-
-	CC_EX_DLL void sxtwl_Time_setMin(sxtwl_Time *time, double value)
-	{
-		time->min = value;
-	}
-
-	CC_EX_DLL void sxtwl_Time_setSec(sxtwl_Time *time, double value)
-	{
-		time->sec = value;
-	}
-
-	//=====================================================
-	void *struct_malloc(int size)
-	{
-		return malloc((size_t)size);
-	}
-
-	void struct_free(void *ptr)
-	{
-		free(ptr);
-	}
-	//=====================================================
+    ret->ptr = (Day *)((Day *)day->ptr)->after(day_num);
+    return ret;
 }
 
-#ifdef __EMSCRIPTEN__
-int main()
+sxtwl_Day *sxtwl_before(sxtwl_Day *day, sxtwl_Day *ret, int day_num)
 {
-	return 0;
+    ret->ptr = (Day *)((Day *)day->ptr)->before(day_num);
+    return ret;
 }
-#endif
+
+int sxtwl_getLunarMonth(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getLunarMonth();
+}
+
+int sxtwl_getLunarDay(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getLunarDay();
+}
+
+int sxtwl_getLunarYear(sxtwl_Day *day, bool chineseNewYearBoundary)
+{
+    return ((Day *)day->ptr)->getLunarYear(chineseNewYearBoundary);
+}
+
+void *sxtwl_getYearGZ(void *GZPtr, sxtwl_Day *day, bool chineseNewYearBoundary)
+{
+    auto ret = (GZ *)GZPtr;
+    *ret = ((Day *)day->ptr)->getYearGZ(chineseNewYearBoundary);
+    return ret;
+}
+
+void *sxtwl_getMonthGZ(void *GZPtr, sxtwl_Day *day)
+{
+    auto ret = (GZ *)GZPtr;
+    *ret = ((Day *)day->ptr)->getMonthGZ();
+    return ret;
+}
+
+void *sxtwl_getDayGZ(void *GZPtr, sxtwl_Day *day)
+{
+    auto ret = (GZ *)GZPtr;
+    *ret = ((Day *)day->ptr)->getDayGZ();
+
+    return ret;
+}
+
+void *sxtwl_getHourGZ(void *GZPtr, sxtwl_Day *day, uint8_t hour, bool isZaoWanZiShi)
+{
+    auto ret = (GZ *)GZPtr;
+    *ret = ((Day *)day->ptr)->getHourGZ(hour, isZaoWanZiShi);
+
+    return ret;
+}
+
+bool sxtwl_isLunarLeap(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->isLunarLeap();
+}
+
+int sxtwl_getSolarYear(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getSolarYear();
+}
+
+uint8_t sxtwl_getSolarMonth(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getSolarMonth();
+}
+
+int sxtwl_getSolarDay(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getSolarDay();
+}
+
+uint8_t sxtwl_getWeek(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getWeek();
+}
+
+// 处于该月的第几周
+uint8_t sxtwl_getWeekIndex(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getWeekIndex();
+}
+
+// 是否有节气
+bool sxtwl_hasJieQi(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->hasJieQi();
+}
+
+// 获取节气
+uint8_t sxtwl_getJieQi(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getJieQi();
+}
+
+double sxtwl_getJieQiJD(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getJieQiJD();
+}
+
+// 获取星座
+uint8_t sxtwl_getConstellation(sxtwl_Day *day)
+{
+    return ((Day *)day->ptr)->getConstellation();
+}
+
+sxtwl_Day *sxtwl_newDay()
+{
+    sxtwl_Day *day = new sxtwl_Day();
+    day->ptr = NULL;
+    return day;
+}
+
+void sxtwl_freeDay(sxtwl_Day *day)
+{
+    if (day->ptr != NULL)
+    {
+        delete ((Day *)day->ptr);
+    }
+    delete day;
+}
+
+void *sxtwl_getShiGz(void  *GZPtr, uint8_t dayTg, uint8_t hour, bool isZaoWanZiShi)
+{
+    auto ret = (GZ *)GZPtr;
+    *ret = sxtwl::getShiGz(dayTg, hour, isZaoWanZiShi);
+    return ret;
+}
+
+uint8_t sxtwl_getRunMonth(int By)
+{
+    return sxtwl::getRunMonth(By);
+}
+
+uint8_t sxtwl_getLunarMonthNum(int By, uint8_t month, bool isRun)
+{
+    return sxtwl::getLunarMonthNum(By, month, isRun);
+}
+
+sxtwl_Time sxtwl_JD2DD(double jd)
+{
+    Time t = sxtwl::JD2DD(jd);
+    sxtwl_Time time;
+    time.year = t.Y;
+    time.month = t.M;
+    time.day = t.D;
+
+    time.hour = t.h;
+    time.min = t.m;
+    time.sec = t.s;
+    return time;
+}
+
+double sxtwl_toJD(sxtwl_Time *time)
+{
+    Time t(time->year, time->month, time->day, time->hour, time->min, time->sec);
+    return sxtwl::toJD(t);
+}
+
+sxtwl_Day *sxtwl_fromSolar(sxtwl_Day *ret, int year, uint8_t month, int day)
+{
+    ret->ptr = (void *)sxtwl::fromSolar(year, month, day);
+    return ret;
+}
+
+sxtwl_Day *sxtwl_fromLunar(sxtwl_Day *ret, int year, uint8_t month, int day, bool isRun)
+{
+    ret->ptr = (void *)sxtwl::fromLunar(year, month, day, isRun);
+    return ret;
+}
+
+void *sxtwl_siZhu2Year(void *JdListPtr, void *yearGZ, void *yueGZ, void *riGZ, void *shiGZ, int fromYear, int toYear)
+{
+    auto ret = (std::vector<double> *)JdListPtr;
+    *ret = sxtwl::siZhu2Year(*((GZ *)yearGZ), *((GZ *)yueGZ), *((GZ *)riGZ), *((GZ *)shiGZ), fromYear, toYear);
+    return ret;
+}
+
+void *JdList_new()
+{
+    auto ret = new std::vector<double>();
+    return ret;
+}
+void JdList_free(void *ptr)
+{
+    auto ret = (std::vector<double> *)ptr;
+    delete ret;
+}
+int JdList_getNum(void *ptr)
+{
+    auto ret = (std::vector<double> *)ptr;
+    return ret->size();
+}
+
+double JdList_indexOf(void *ptr, int index)
+{
+    auto ret = (std::vector<double> *)ptr;
+    return ret->at(index);
+}
+
+void *GZ_new(int tg, int dz)
+{
+    return new GZ(tg, dz);
+}
+void GZ_free(void *ptr)
+{
+    auto ret = (GZ *)ptr;
+    delete ret;
+}
+int GZ_getTg(void *ptr)
+{
+    auto ret = (GZ *)ptr;
+    return ret->tg;
+}
+void GZ_setTg(void *ptr, int tg)
+{
+    auto ret = (GZ *)ptr;
+    ret->tg = tg;
+}
+int GZ_getDz(void *ptr)
+{
+    auto ret = (GZ *)ptr;
+    return ret->dz;
+}
+void GZ_setDz(void *ptr, int dz)
+{
+    auto ret = (GZ *)ptr;
+    ret->dz = dz;
+}
+EXTERN_C_END
--- c/sxtwl_c.h
+++ c/sxtwl_c.h
@@ -1,150 +1,109 @@
-#ifndef _SXTWL_C_H_
-#define _SXTWL_C_H_
-
-#define _USREXDLL 1
-
-#if (defined(WIN32) && defined(_WINDOWS)) || defined(WINRT) || defined(WP8)
-#ifdef __MINGW32__
-#include <string.h>
-#endif
-
-#if defined(CC_STATIC)
-#define CC_EX_DLL
-#else
-#if defined(_USREXDLL)
-#define CC_EX_DLL __declspec(dllexport)
-#else /* use a DLL library */
-#define CC_EX_DLL __declspec(dllimport)
-#endif
-#endif
-
-/* Define NULL pointer value */
-#ifndef NULL
-#ifdef __cplusplus
-#define NULL 0
-#else
-#define NULL ((void *)0)
-#endif
-#endif
-#elif defined(_SHARED_)
-#define CC_EX_DLL __attribute__((visibility("default")))
-#else
-#define CC_EX_DLL
-#endif
-
-#ifdef __EMSCRIPTEN__
-
-#ifdef CC_EX_DLL
-#undef CC_EX_DLL
-#endif
-
-#include <emscripten/emscripten.h>
-#include <emscripten/bind.h>
-using namespace emscripten;
-#define CC_EX_DLL EMSCRIPTEN_KEEPALIVE
-#endif
+#pragma once
 
 #include <stdint.h>
 
-extern "C"
-{
-#pragma pack(push, 1)
-	typedef struct sxtwl_Time
-	{
-		int year, month, day;
-		double hour, min, sec;
-	} sxtwl_Time;
-#pragma pack(pop)
+#include <stdbool.h>
 
-#pragma pack(push, 1)
-	typedef struct sxtwl_GZ
-	{
-		uint8_t tg;
-		uint8_t dz;
-	} sxtwl_GZ;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-	typedef struct sxtwl_Day
-	{
-		void *ptr;
-	} sxtwl_Day;
-#pragma pack(pop)
-
-	// 注意返回的sxtwl_Day指针，需要使用sxtwl_freeDay释放
-	CC_EX_DLL sxtwl_Day *sxtwl_after(sxtwl_Day *day, sxtwl_Day *ret, int day_num);
-	CC_EX_DLL sxtwl_Day *sxtwl_before(sxtwl_Day *day, sxtwl_Day *ret, int day_num);
-
-	CC_EX_DLL int sxtwl_getLunarDay(sxtwl_Day *day);
-	CC_EX_DLL int sxtwl_getLunarYear(sxtwl_Day *day, bool chineseNewYearBoundary);
-	CC_EX_DLL sxtwl_GZ *sxtwl_getYearGZ(sxtwl_Day *day, sxtwl_GZ *ret, bool chineseNewYearBoundary);
-	CC_EX_DLL sxtwl_GZ *sxtwl_getMonthGZ(sxtwl_Day *day, sxtwl_GZ *ret);
-	CC_EX_DLL sxtwl_GZ *sxtwl_getDayGZ(sxtwl_Day *day, sxtwl_GZ *ret);
-	// 注意非早晚子时的时候，day要算第二天
-	CC_EX_DLL sxtwl_GZ *sxtwl_getHourGZ(sxtwl_Day *day, sxtwl_GZ *ret, uint8_t hour, bool isZaoWanZiShi);
-	CC_EX_DLL bool sxtwl_isLunarLeap(sxtwl_Day *day);
-
-	CC_EX_DLL int sxtwl_getSolarYear(sxtwl_Day *day);
-	CC_EX_DLL uint8_t sxtwl_getSolarMonth(sxtwl_Day *day);
-	CC_EX_DLL int sxtwl_getSolarDay(sxtwl_Day *day);
-	CC_EX_DLL uint8_t sxtwl_getWeek(sxtwl_Day *day);
-	// 处于该月的第几周
-	CC_EX_DLL uint8_t sxtwl_getWeekIndex(sxtwl_Day *day);
-	//是否有节气
-	CC_EX_DLL bool sxtwl_hasJieQi(sxtwl_Day *day);
-	// 获取节气
-	CC_EX_DLL uint8_t sxtwl_getJieQi(sxtwl_Day *day);
-	CC_EX_DLL double sxtwl_getJieQiJD(sxtwl_Day *day);
-	// 获取星座
-	CC_EX_DLL uint8_t sxtwl_getConstellation(sxtwl_Day *day);
-	CC_EX_DLL void sxtwl_freeDay(sxtwl_Day *day);
-
-	CC_EX_DLL sxtwl_Day *sxtwl_fromSolar(sxtwl_Day *ret, int year, uint8_t month, int day);
-
-	CC_EX_DLL sxtwl_Day *sxtwl_fromLunar(sxtwl_Day *ret, int year, uint8_t month, int day, bool isRun);
-
-	//通过四柱获取年月日, 返回的是儒略日列表
-	CC_EX_DLL double *sxtwl_siZhu2Year(double **list, int &ret_num, sxtwl_GZ *year, sxtwl_GZ *yue, sxtwl_GZ *ri, sxtwl_GZ *shi, int fromYear, int toYear);
-	////获取时辰上的那个天干
-	CC_EX_DLL sxtwl_GZ *sxtwl_getShiGz(uint8_t dayTg, sxtwl_GZ *ret, uint8_t hour, bool isZaoWanZiShi);
-	//获取一年中的润月(不存，则返回0)
-	CC_EX_DLL uint8_t sxtwl_getRunMonth(int By);
-	//获取一月中的阴日数量
-	CC_EX_DLL uint8_t sxtwl_getLunarMonthNum(int By, uint8_t month, bool isRun);
-	//儒略日数转公历
-	CC_EX_DLL sxtwl_Time *sxtwl_JD2DD(double jd, sxtwl_Time *ret);
-	//公历转儒略日
-	CC_EX_DLL double sxtwl_toJD(sxtwl_Time *time);
-
-	//==================================================================================
-	CC_EX_DLL void *struct_malloc(int size);
-	CC_EX_DLL void struct_free(void *);
-
-	CC_EX_DLL sxtwl_GZ *sxtwl_newGZ();
-	CC_EX_DLL void sxtwl_freeGZ(sxtwl_GZ *gz);
-	CC_EX_DLL uint8_t sxtwl_GZ_getTg(sxtwl_GZ *gz);
-	CC_EX_DLL uint8_t sxtwl_GZ_getDz(sxtwl_GZ *gz);
-	CC_EX_DLL void sxtwl_GZ_setTg(sxtwl_GZ *gz, uint8_t tg);
-	CC_EX_DLL void sxtwl_GZ_setDz(sxtwl_GZ *gz, uint8_t dz);
-
-	CC_EX_DLL sxtwl_Time *sxtwl_newTime();
-	CC_EX_DLL void sxtwl_freeTime(sxtwl_Time *time);
-
-	CC_EX_DLL int sxtwl_Time_getYear(sxtwl_Time *time);
-	CC_EX_DLL int sxtwl_Time_getMonth(sxtwl_Time *time);
-	CC_EX_DLL int sxtwl_Time_getDay(sxtwl_Time *time);
-
-	CC_EX_DLL void sxtwl_Time_setYear(sxtwl_Time *time, int value);
-	CC_EX_DLL void sxtwl_Time_setMonth(sxtwl_Time *time, int value);
-	CC_EX_DLL void sxtwl_Time_setDay(sxtwl_Time *time, int value);
-
-	CC_EX_DLL double sxtwl_Time_getHour(sxtwl_Time *time);
-	CC_EX_DLL double sxtwl_Time_getMin(sxtwl_Time *time);
-	CC_EX_DLL double sxtwl_Time_getSec(sxtwl_Time *time);
-
-	CC_EX_DLL void sxtwl_Time_setHour(sxtwl_Time *time, double value);
-	CC_EX_DLL void sxtwl_Time_setMin(sxtwl_Time *time, double value);
-	CC_EX_DLL void sxtwl_Time_setSec(sxtwl_Time *time, double value);
-	//==================================================================================
-}
+#if _WIN32
+#define FFI_PLUGIN_EXPORT __declspec(dllexport)
+#else
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#define FFI_PLUGIN_EXPORT EMSCRIPTEN_KEEPALIVE
+#else
+#define FFI_PLUGIN_EXPORT
 #endif
+#endif
+
+#ifdef __cplusplus
+#define EXTERN_C_BEGIN \
+    extern "C"         \
+    {
+#else
+#define EXTERN_C_BEGIN
+#endif
+
+#ifdef __cplusplus
+#define EXTERN_C_END }
+#else
+#define EXTERN_C_END
+#endif
+
+EXTERN_C_BEGIN
+
+typedef struct sxtwl_Time
+{
+    int year, month, day;
+    double hour, min, sec;
+} sxtwl_Time;
+
+typedef struct sxtwl_Day
+{
+    void *ptr;
+} sxtwl_Day;
+
+// 内存管理
+FFI_PLUGIN_EXPORT sxtwl_Day *sxtwl_newDay();
+FFI_PLUGIN_EXPORT void sxtwl_freeDay(sxtwl_Day *day);
+
+// 注意返回的sxtwl_Day指针，需要使用sxtwl_freeDay释放
+FFI_PLUGIN_EXPORT sxtwl_Day *sxtwl_after(sxtwl_Day *day, sxtwl_Day *ret, int day_num);
+FFI_PLUGIN_EXPORT sxtwl_Day *sxtwl_before(sxtwl_Day *day, sxtwl_Day *ret, int day_num);
+
+FFI_PLUGIN_EXPORT int sxtwl_getLunarMonth(sxtwl_Day *day);
+FFI_PLUGIN_EXPORT int sxtwl_getLunarDay(sxtwl_Day *day);
+FFI_PLUGIN_EXPORT int sxtwl_getLunarYear(sxtwl_Day *day, bool chineseNewYearBoundary);
+
+FFI_PLUGIN_EXPORT void *sxtwl_getYearGZ(void *GZPtr, sxtwl_Day *day, bool chineseNewYearBoundary);
+FFI_PLUGIN_EXPORT void *sxtwl_getMonthGZ(void *GZPtr, sxtwl_Day *day);
+FFI_PLUGIN_EXPORT void *sxtwl_getDayGZ(void *GZPtr, sxtwl_Day *day);
+// 注意非早晚子时的时候，day要算第二天
+FFI_PLUGIN_EXPORT void *sxtwl_getHourGZ(void *GZPtr, sxtwl_Day *day, uint8_t hour, bool isZaoWanZiShi);
+FFI_PLUGIN_EXPORT bool sxtwl_isLunarLeap(sxtwl_Day *day);
+
+FFI_PLUGIN_EXPORT int sxtwl_getSolarYear(sxtwl_Day *day);
+FFI_PLUGIN_EXPORT uint8_t sxtwl_getSolarMonth(sxtwl_Day *day);
+FFI_PLUGIN_EXPORT int sxtwl_getSolarDay(sxtwl_Day *day);
+FFI_PLUGIN_EXPORT uint8_t sxtwl_getWeek(sxtwl_Day *day);
+// 处于该月的第几周
+FFI_PLUGIN_EXPORT uint8_t sxtwl_getWeekIndex(sxtwl_Day *day);
+// 是否有节气
+FFI_PLUGIN_EXPORT bool sxtwl_hasJieQi(sxtwl_Day *day);
+// 获取节气
+FFI_PLUGIN_EXPORT uint8_t sxtwl_getJieQi(sxtwl_Day *day);
+FFI_PLUGIN_EXPORT double sxtwl_getJieQiJD(sxtwl_Day *day);
+// 获取星座
+FFI_PLUGIN_EXPORT uint8_t sxtwl_getConstellation(sxtwl_Day *day);
+
+FFI_PLUGIN_EXPORT sxtwl_Day *sxtwl_fromSolar(sxtwl_Day *ret, int year, uint8_t month, int day);
+
+FFI_PLUGIN_EXPORT sxtwl_Day *sxtwl_fromLunar(sxtwl_Day *ret, int year, uint8_t month, int day, bool isRun);
+
+// 通过四柱获取年月日, 返回的是儒略日列表
+FFI_PLUGIN_EXPORT void *sxtwl_siZhu2Year(void *JdListPtr, void *yearGZ, void *yueGZ, void *riGZ, void *shiGZ, int fromYear, int toYear);
+////获取时辰上的那个天干
+FFI_PLUGIN_EXPORT void *sxtwl_getShiGz(void *gzPtr, uint8_t dayTg, uint8_t hour, bool isZaoWanZiShi);
+
+// 获取一年中的润月(不存，则返回0)
+FFI_PLUGIN_EXPORT uint8_t sxtwl_getRunMonth(int By);
+// 获取一月中的阴日数量
+FFI_PLUGIN_EXPORT uint8_t sxtwl_getLunarMonthNum(int By, uint8_t month, bool isRun);
+// 儒略日数转公历
+FFI_PLUGIN_EXPORT sxtwl_Time sxtwl_JD2DD(double jd);
+// 公历转儒略日
+FFI_PLUGIN_EXPORT double sxtwl_toJD(sxtwl_Time *time);
+
+// 反推的jd列表使用
+FFI_PLUGIN_EXPORT void *JdList_new();
+FFI_PLUGIN_EXPORT void JdList_free(void *ptr);
+FFI_PLUGIN_EXPORT int JdList_getNum(void *ptr);
+FFI_PLUGIN_EXPORT double JdList_indexOf(void *ptr, int index);
+
+FFI_PLUGIN_EXPORT void *GZ_new(int tg, int dz);
+FFI_PLUGIN_EXPORT void GZ_free(void *ptr);
+FFI_PLUGIN_EXPORT int GZ_getTg(void *ptr);
+FFI_PLUGIN_EXPORT void GZ_setTg(void *ptr, int tg);
+FFI_PLUGIN_EXPORT int GZ_getDz(void *ptr);
+FFI_PLUGIN_EXPORT void GZ_setDz(void *ptr, int dz);
+
+EXTERN_C_END
